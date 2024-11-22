a89cfe1451f705147c12c97cab52ec70a0319d4b
bobbyfranklin954 <bobbyfranklin954@gmail.com>
2024-11-22 16:39:12 -0500

    Completed main app | Added footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>© B5 Event Planners Inc. ™</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
